6c53d7ec1cb06ad6424c7e1e690dfdfd1b5c08d5
Lukasz905b <lukasz905b@gmail.com>
2023-03-15 17:16:43 +0100

    add initial spring boot project files

---
+++ spring-boot-VMM/src/main/java/pp/project/vmm/VmmApplication.java
@@ -0,0 +1,13 @@
+package pp.project.vmm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VmmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VmmApplication.class, args);
+	}
+
+}
