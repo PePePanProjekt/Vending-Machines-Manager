11d50336276c32f507ac2e4ea9c5129fc08dd51c
Sebastian Fijałkowski <fijalkowskiweb@gmail.com>
2023-03-29 14:03:24 +0200

    homa, add-machine design changes

---
+++ angular-VMM/src/app/navbar-guest/navbar-guest.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { NavbarGuestComponent } from './navbar-guest.component';
+
+describe('NavbarGuestComponent', () => {
+  let component: NavbarGuestComponent;
+  let fixture: ComponentFixture<NavbarGuestComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ NavbarGuestComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(NavbarGuestComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
