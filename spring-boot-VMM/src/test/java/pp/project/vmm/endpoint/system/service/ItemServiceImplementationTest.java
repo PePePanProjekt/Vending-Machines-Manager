af730c991e3a91ff6c52b6161d63d93ac7cda662
Roman Gawenda <72347545+RGawenda@users.noreply.github.com>
2023-04-12 22:07:50 +0200

    added tests ItemServiceImplementationTest and MachineServiceImplementationTest

--- spring-boot-VMM/src/test/java/pp/project/vmm/endpoint/system/service/ItemServiceImplementationTest.java
+++ spring-boot-VMM/src/test/java/pp/project/vmm/endpoint/system/service/ItemServiceImplementationTest.java
@@ -1,16 +1,83 @@
 package pp.project.vmm.endpoint.system.service;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.hamcrest.Matchers;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import pp.project.vmm.endpoint.system.model.Item;
+import pp.project.vmm.endpoint.system.repository.ItemRepository;
+import pp.project.vmm.endpoint.system.service.dto.ItemDetailsDTO;
+
+import static java.lang.Boolean.TRUE;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.BDDMockito.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 
-class ItemServiceTest {
+@ExtendWith(SpringExtension.class)
 
-    @Test
-    void getItems() {
+class ItemServiceImplementationTest {
+    @Mock
+    ItemRepository itemRepository;
+    @InjectMocks
+    ItemServiceImplementation itemService;
+    UUID id = UUID.randomUUID();
+    @BeforeEach
+    public void init()
+    {
+        given(itemRepository.findAll()).willReturn(prepareMockData());
+        given(itemRepository.findById(id)).willReturn(prepareMockData2(id));
+        given(itemRepository.existsById(id)).willReturn(TRUE);
+
     }
 
     @Test
-    void getItemById() {
+    void getItemsTest() {
+
+        List<ItemDetailsDTO> itemDetailsDTO = itemService.getItems();
+
+        assertThat(itemDetailsDTO, Matchers.hasSize(3));
+    }
+
+    @Test
+    void getItemsTest2() {
+
+        List<ItemDetailsDTO> itemDetailsDTO = itemService.getItems();
+        String nameItem = String.valueOf(itemDetailsDTO.get(0).getName());
+        assertEquals(nameItem, "name1");
+    }
+
+    private List<Item> prepareMockData(){
+        List<Item> item = new ArrayList<>();
+
+        item.add(new Item("name1", 1));
+        item.add(new Item("name2", 2));
+        item.add(new Item("name3", 3));
+
+        return item;
+    }
+
+    private Optional<Item> prepareMockData2(UUID id){
+        Item item = new Item("name1", 1);
+        return Optional.of(item);
+    }
+
+
+    @Test
+    void getItemByIdTest() {
+        ItemDetailsDTO itemDetailsDTO = itemService.getItemById(id);
+        String nameItem = itemDetailsDTO.getName();
+
+        assertEquals(nameItem, "name1");
     }
 
     @Test
