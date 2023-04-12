a0fa1cee410f15c25795d1655507a5efd5d27dfa
Roman Gawenda <72347545+RGawenda@users.noreply.github.com>
2023-04-12 15:46:19 +0200

    added templates for tests and added dependency assertj

---
+++ spring-boot-VMM/src/test/java/pp/project/vmm/endpoint/system/service/ItemServiceImplementationTest.java
@@ -0,0 +1,27 @@
+package pp.project.vmm.endpoint.system.service;
+
+import org.junit.jupiter.api.Test;
+
+
+class ItemServiceTest {
+
+    @Test
+    void getItems() {
+    }
+
+    @Test
+    void getItemById() {
+    }
+
+    @Test
+    void updateItem() {
+    }
+
+    @Test
+    void addItem() {
+    }
+
+    @Test
+    void deleteItemById() {
+    }
+}
---
+++ spring-boot-VMM/src/test/java/pp/project/vmm/endpoint/system/service/MachineServiceImplementationTest.java
@@ -0,0 +1,36 @@
+package pp.project.vmm.endpoint.system.service;
+
+import org.junit.jupiter.api.Test;
+
+
+
+class MachineServiceImplementationTest {
+
+    @Test
+    void getMachinesInfoSimple() {
+    }
+
+    @Test
+    void getMachineInfoById() {
+    }
+
+    @Test
+    void addMachine() {
+    }
+
+    @Test
+    void updateMachine() {
+    }
+
+    @Test
+    void refillMachine() {
+    }
+
+    @Test
+    void deleteMachine() {
+    }
+
+    @Test
+    void deleteMachineById() {
+    }
+}
