887e8a6c765ccf4c387df39883ffbc0d2bebb093
Roman Gawenda <72347545+RGawenda@users.noreply.github.com>
2023-04-15 22:24:13 +0200

    added tests to updateItem() method

--- spring-boot-VMM/src/test/java/pp/project/vmm/endpoint/system/service/ItemServiceImplementationTest.java
+++ spring-boot-VMM/src/test/java/pp/project/vmm/endpoint/system/service/ItemServiceImplementationTest.java
@@ -8,14 +8,18 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import pp.project.vmm.endpoint.system.model.Item;
 import pp.project.vmm.endpoint.system.repository.ItemRepository;
 import pp.project.vmm.endpoint.system.service.dto.ItemDetailsDTO;
+import pp.project.vmm.endpoint.system.service.dto.ItemSimpleDTO;
 
 import static java.lang.Boolean.TRUE;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.BDDMockito.*;
 import java.util.ArrayList;
 import java.util.List;
@@ -81,7 +85,38 @@ class ItemServiceImplementationTest {
     }
 
     @Test
-    void updateItem() {
+    void shouldUpdateItem() {
+        ItemSimpleDTO simpleDTO = new ItemSimpleDTO(id, "name1");
+        Item item = new Item("item1", 10);
+        given(itemRepository.existsById(simpleDTO.getId())).willReturn(true);
+        given(itemRepository.findById(simpleDTO.getId())).willReturn(Optional.of(item));
+
+        ResponseEntity<String> resEntity =  itemService.updateItem(simpleDTO);
+
+        verify(itemRepository, times(1)).existsById(simpleDTO.getId());
+        verify(itemRepository, times(1)).findById(simpleDTO.getId());
+
+        assertNotNull(resEntity);
+        assertEquals(HttpStatus.OK, resEntity.getStatusCode());
+        assertEquals("Successfully updated item", resEntity.getBody());
+    }
+
+    @Test
+    public void shouldNotUpdateItem() {
+
+        ItemSimpleDTO simpleDTO = new ItemSimpleDTO(id, "item1");
+
+        given(itemRepository.existsById(simpleDTO.getId())).willReturn(false);
+
+        ResponseEntity<String> resEntity = itemService.updateItem(simpleDTO);
+
+        verify(itemRepository, times(1)).existsById(simpleDTO.getId());
+        verify(itemRepository, never()).findById(simpleDTO.getId());
+        verify(itemRepository, never()).save(any());
+
+        assertNotNull(resEntity);
+        assertEquals(HttpStatus.NOT_FOUND, resEntity.getStatusCode());
+        assertEquals("Item of given id does not exist", resEntity.getBody());
     }
 
     @Test
