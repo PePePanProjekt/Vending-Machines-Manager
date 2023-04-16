b54de4171a3ef30d06497489e8f6870b3876922a
Roman Gawenda <72347545+RGawenda@users.noreply.github.com>
2023-04-16 20:05:42 +0200

    added tests to getMachineInfoById() method

--- spring-boot-VMM/src/test/java/pp/project/vmm/endpoint/system/service/MachineServiceTest.java
+++ spring-boot-VMM/src/test/java/pp/project/vmm/endpoint/system/service/MachineServiceTest.java
@@ -6,18 +6,21 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import pp.project.vmm.endpoint.system.model.Contains;
+import pp.project.vmm.endpoint.system.model.Item;
 import pp.project.vmm.endpoint.system.model.VendingMachine;
 import pp.project.vmm.endpoint.system.repository.ContainsRepository;
 import pp.project.vmm.endpoint.system.repository.ItemRepository;
 import pp.project.vmm.endpoint.system.repository.VendingMachineRepository;
+import pp.project.vmm.endpoint.system.service.dto.VendingMachineFullInfoDTO;
 import pp.project.vmm.endpoint.system.service.dto.VendingMachineSimpleDTO;
+import pp.project.vmm.endpoint.system.service.dto.VendingMachineSlotDTO;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.BDDMockito.given;
 
 @ExtendWith(SpringExtension.class)
@@ -98,12 +101,82 @@ class MachineServiceImplementationTest {
         assertEquals(id2, result4.getId());
         assertEquals("Location2", result4.getLocation());
         assertEquals("Machine2", result4.getName());
-
     }
 
     @Test
-    void getMachineInfoById() {
+    public void shouldGetMachineInfoById() {
+        // Given
+        VendingMachine vendingMachine = new VendingMachine();
+        vendingMachine.setId(id);
+        vendingMachine.setLocation("Location1");
+        vendingMachine.setName("Machine1");
+        vendingMachine.setDispenserAmount(5);
+        vendingMachine.setDispenserDepth(10);
+
+        Contains contains1 = new Contains();
+        contains1.setDispenserNumber(1);
+        contains1.setItemPrice(1.5f);
+        contains1.setItemAmount(20);
+        Item item1 = new Item();
+        item1.setId(UUID.randomUUID());
+        item1.setName("Item1");
+        contains1.setItem(item1);
+
+        Contains contains2 = new Contains();
+        contains2.setDispenserNumber(2);
+        contains2.setItemPrice(2.0f);
+        contains2.setItemAmount(10);
+        Item item2 = new Item();
+        item2.setId(UUID.randomUUID());
+        item2.setName("Item2");
+        contains2.setItem(item2);
+
+        vendingMachine.setContains(Arrays.asList(contains1, contains2));
+
+        given(vendingMachineRepository.findById(id)).willReturn(Optional.of(vendingMachine));
+
+        // When
+        VendingMachineFullInfoDTO result = vendingMachineService.getMachineInfoById(id);
+
+        // Then
+        assertNotNull(result);
+        assertEquals(id, result.getDetails().getId());
+        assertEquals("Location1", result.getDetails().getLocation());
+        assertEquals("Machine1", result.getDetails().getName());
+        assertEquals(5, result.getDetails().getDispenserAmount());
+        assertEquals(10, result.getDetails().getDispenserDepth());
+
+        List<VendingMachineSlotDTO> slotDTOList = result.getSlots();
+        assertNotNull(slotDTOList);
+        assertEquals(2, slotDTOList.size());
+
+        VendingMachineSlotDTO result1 = slotDTOList.get(0);
+        assertEquals(1, result1.getSlotNumber());
+        assertEquals(item1.getId(), result1.getItemId());
+        assertEquals("Item1", result1.getItemName());
+        assertEquals(1.5, result1.getItemPrice(), 0.001);
+        assertEquals(20, result1.getItemAmount());
+
+        VendingMachineSlotDTO result2 = slotDTOList.get(1);
+        assertEquals(2, result2.getSlotNumber());
+        assertEquals(item2.getId(), result2.getItemId());
+        assertEquals("Item2", result2.getItemName());
+        assertEquals(2.0, result2.getItemPrice(), 0.001);
+        assertEquals(10, result2.getItemAmount());
     }
+
+    @Test
+    public void shouldGetMachineInfoByIdWithNonExistingId() {
+        // Given
+        given(vendingMachineRepository.findById(id)).willReturn(Optional.empty());
+
+        // When
+        VendingMachineFullInfoDTO result = vendingMachineService.getMachineInfoById(id);
+
+        // Then
+        assertNull(result);
+    }
+
 
     @Test
     void addMachine() {
