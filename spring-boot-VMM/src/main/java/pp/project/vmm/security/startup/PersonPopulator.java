91db07030a322878e29d82596ac69bf438baf1a2
Sebastian Fijałkowski <fijalkowskiweb@gmail.com>
2023-04-19 11:20:14 +0200

    configuration changes

--- spring-boot-VMM/src/main/java/pp/project/vmm/security/startup/PersonPopulator.java
+++ spring-boot-VMM/src/main/java/pp/project/vmm/security/startup/PersonPopulator.java
@@ -1,18 +1,16 @@
 package pp.project.vmm.security.startup;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 import pp.project.vmm.security.person.Person;
 import pp.project.vmm.security.person.PersonRepository;
 
 @Component
+@RequiredArgsConstructor
 public class PersonPopulator implements CommandLineRunner {
 
     private final PersonRepository personRepository;
-
-    public PersonPopulator(PersonRepository personRepository) {
-        this.personRepository = personRepository;
-    }
 
     @Override
     public void run(String... args) {
